1ba42080c3ef8fc631c44514d210f575e1baebde
zhayujie <yjzha1996@163.com>
2020-05-06 11:18:17 +0800

    feat: use chinese archive date

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * 这是 Typecho 0.9 系统的一套默认皮肤
+ * 基于默认主题自定义开发的版本
  * 
- * @package Typecho Replica Theme 
- * @author Typecho Team
- * @version 1.2
- * @link http://typecho.org
+ * @package 自定义开发主题
+ * @author zhayujie
+ * @version 2.0
+ * @link https://zhayujie.com
  */
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
@@ -13,16 +13,16 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  ?>
 
 <div class="col-mb-12 col-8" id="main" role="main">
-	<?php while($this->next()): ?>
+    <?php while($this->next()): ?>
         <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
 			<h2 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
 			<ul class="post-meta">
-				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
-				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
-				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
-                                <li>阅读: <?php echo ViewsCounter_Plugin::getViews(); ?> 次</li>
-			</ul>
+                <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
+                <li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
+                <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
+                <li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
+                <li>阅读: <?php echo ViewsCounter_Plugin::getViews(); ?> 次</li>
+            </ul>
             <div class="post-content" itemprop="articleBody">
     			<?php $this->content('- 阅读剩余部分 -'); ?>
             </div>
--- sidebar.php
+++ sidebar.php
@@ -33,7 +33,7 @@
     <section class="widget">
 		<h3 class="widget-title"><?php _e('归档'); ?></h3>
         <ul class="widget-list">
-            <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')
+            <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=Y年m月')
             ->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
         </ul>
 	</section>
@@ -44,14 +44,13 @@
 		<h3 class="widget-title"><?php _e('其它'); ?></h3>
         <ul class="widget-list">
             <?php if($this->user->hasLogin()): ?>
-				<li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
+				<!-- <li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li> -->
                 <li><a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a></li>
             <?php else: ?>
-                <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li>
+                <!-- <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li> -->
             <?php endif; ?>
             <li><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章 RSS'); ?></a></li>
             <li><a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论 RSS'); ?></a></li>
-            <li><a href="http://www.typecho.org">Typecho</a></li>
         </ul>
 	</section>
     <?php endif; ?>
